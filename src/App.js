d6c00b0973c05cb351cee0c455620e565e00ebb9
kritydangol <krity.dangol1@gmail.com>
2022-09-19 09:01:41 +0545

    message

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from "./logo.svg";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">
+        <Navbar />
+        <div className="container">
+          <article>
+            <h1>Lets Get This Bread </h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry...
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
