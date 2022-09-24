a617e3849239f3db23891b7d3b89331bb8a300c0
krity <kritydangol1@gmail.com>
2022-09-25 00:35:23 +0545

    stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,27 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h5>
-        <b>SoftBread Tech</b>
-      </h5>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Aliquam ultrices
-        sagittis orci a. Molestie nunc non blandit massa enim nec. Senectus et
-        netus et malesuada fames ac turpis egestas.
-      </p>
+      <div className="footer-col-4">
+        <h5>
+          <b>SoftBread Tech</b>
+        </h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam
+          ultrices sagittis orci a. Molestie nunc non blandit massa enim nec.
+          Senectus et netus et malesuada fames ac turpis egestas.
+        </p>
+        <div>
+          <i class="fa fa-facebook fa-footer" aria-hidden="true"></i>
+          <i class="fa fa-instagram fa-footer" aria-hidden="true"></i>
+          <i class="fa fa-twitter fa-footer" aria-hidden="true"></i>
+          <i class="fa fa-youtube-play fa-footer" aria-hidden="true"></i>
+          <i class=" fa fa-whatsapp fa-footer" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div className="footer-col-4">
+        
+      </div>
     </div>
   );
 };
