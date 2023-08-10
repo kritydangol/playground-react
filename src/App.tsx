2980704afaec071e9381903959f655f744089269
kritydangol <kritydangol1@gmail.com>
2023-08-10 17:37:45 +0545

    updates input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/inputField";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="heading">Tasks</h1>
+      <InputField />
     </div>
   );
 };
---
+++ src/components/inputField.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <form className="input">
+      <input
+        type="input"
+        id="a"
+        className="input_field"
+        placeholder="Enter a Task"
+      />
+    </form>
+  );
+};
+
+export default InputField;
